7e81edf3d9af862fe8fc60747799b2c241724c5c
umrohfixaman <umrohfixaman@gmail.com>
2026-03-01 10:41:58 +0700

    Update and rename pwasimpeltoko.js to tokoku.js

--- tokoku.js
+++ tokoku.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjPxnzgW0OmNC3wPeY8Ob56_JtuqgpHpyJS2om_RNvllbQ9yHgbfYp3whOXSXPCbiP-hTc50bGY9Z79wss2WSKMjf1P8sgFDVdgVHqLr4QKqJizKqPi0gVEtXbcAqBFDl29KDKfigQL4iEUL1DVm9o2QLF9sn2_JC2EixEnrrvVKmOBZ1mwPwHMqa1ISyA/s320/ChatGPT%20Image%2026%20Feb%202026,%2007.51.00.png"
       ]);
     })
   );
@@ -19,3 +19,4 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
